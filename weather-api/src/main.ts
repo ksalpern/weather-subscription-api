033a28d6af56cb37dcc6474fcfbe6c99fd797cf7
Kseniia <ksenia.pidopryhora@gmail.com>
2025-05-14 19:32:46 +0300

    cleanup

--- weather-api/src/main.ts
+++ weather-api/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { WeatherModule } from './weather/weather.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(WeatherModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- weather-api/src/subscription/dtos/SubscribeWeather.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class SubscribeWeatherDto {
-  @IsString()
-  city: string;
-}
